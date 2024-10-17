5a45a39f57828f6a18ea4c3c99490f0d2e8d1b3a
Wendy <1155107790@link.cuhk.edu.hk>
2024-10-17 16:42:56 +0800

    refactor: combine order & refund to invoice

--- server/controllers/invoiceController.js
+++ server/controllers/invoiceController.js
@@ -1,16 +1,16 @@
 const BaseController = require('./baseController')
-const { Invoice } = require('../models')
+const { Invoice, Partner, InvoiceItem, Product } = require('../models')
 
 
 class InvoiceController extends BaseController {
     constructor(type) {
         super()
         this.type = type
+        this.isOrder = type.includes('Order')
     }
 
     index = async (req, res, next) => {
         try {
-            const isOrder = this.type.includes('Order')
             const options = {
                 where: { type: this.type },
                 include: [
@@ -21,7 +21,7 @@ class InvoiceController extends BaseController {
                     },
                     {
                         model: Invoice,
-                        as: isOrder ? 'refund' : 'order',
+                        as: this.isOrder ? 'refund' : 'order',
                     }
                 ],
                 group: ['Invoice.id'],
@@ -33,6 +33,52 @@ class InvoiceController extends BaseController {
             }
             const invoices = await Invoice.findAll(options)
             return res.send(invoices)
+        } catch (error) {
+            return this.handleError(res, error)
+        }
+    }
+
+    findById = async (id) => {
+        const options = {
+            include: [
+                {
+                    model: Partner,
+                    as: 'partner'
+                },
+                {
+                    model: InvoiceItem,
+                    as: 'invoiceItems',
+                    include: {
+                        model: Product,
+                        as: 'product'
+                    },
+                },
+                {
+                    model: Invoice,
+                    as: this.isOrder ? 'refund' : 'order',
+                    include: {
+                        model: InvoiceItem,
+                        as: 'invoiceItems',
+                    }
+                }
+            ],
+            where: {
+                id: id,
+                type: this.type
+            },
+            order: [
+                [{ model: InvoiceItem, as: 'invoiceItems' }, 'id', 'ASC'],
+                [{ model: Invoice, as: this.isOrder ? 'refund' : 'order' }, { model: InvoiceItem, as: 'invoiceItems' },'id', 'ASC']
+            ]
+        }
+        const invoice = await Invoice.findOne(options)
+        return invoice
+    }
+
+    show = async (req, res, next) => {
+        try {
+            const invoice = await this.findById(req.params.id)
+            return res.send(invoice)
         } catch (error) {
             return this.handleError(res, error)
         }
