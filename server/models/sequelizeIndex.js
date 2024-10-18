5c07ac688b00396c2c7824a3a854665b6f9413b1
Wendy <1155107790@link.cuhk.edu.hk>
2024-10-18 18:40:24 +0800

    fix: db file path

--- server/models/sequelizeIndex.js
+++ server/models/sequelizeIndex.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: '../sales.db',
+    storage: './sales.db',
 })
 
 module.exports = { sequelize }
