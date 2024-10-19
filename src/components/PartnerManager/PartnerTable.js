b1cf1c72c99c8d7a710e2fc66895500841c6c938
Wendy <1155107790@link.cuhk.edu.hk>
2024-10-19 16:28:31 +0800

    fix: redux re-render

--- src/components/PartnerManager/PartnerTable.js
+++ src/components/PartnerManager/PartnerTable.js
@@ -12,21 +12,19 @@ const PartnerTable = ({ partner }) => {
     const handlePageChange = (page) => {
         setCurrentPage(page)
     }
-    const setting = useSelector(state => ({
-        amountSign: state.functionSetting.amountSign.value,
-        ifShowPayment: state.functionSetting.ifShowPayment.value,
-        ifShowDelivered: state.functionSetting.ifShowDelivered.value,
-    }))
+
+    const amountSign = useSelector(state => state.functionSetting.amountSign.value )
+    const ifShowPayment = useSelector(state => state.functionSetting.ifShowPayment.value )
 
     const columns = [
         { title: '序号', fixed: 'left', render: (_, __, idx) => (currentPage - 1) * pageSize + idx + 1 },
         { title: '单号', dataIndex: 'number', },
         { title: '类型', dataIndex: 'type', render: t => <InvoiceTypeTag type={t} /> },
-        { title: '金额', dataIndex: 'amount', render: a => setting.amountSign + a?.toLocaleString() },
-        setting.ifShowPayment ?
+        { title: '金额', dataIndex: 'amount', render: a => amountSign + a?.toLocaleString() },
+        ifShowPayment ?
         { title: '付款', dataIndex: 'paid', render: (_, invoice) => {
             const paid = Decimal(invoice.prepayment||0).plus(invoice.payment||0)
-            return setting.amountSign + paid.toLocaleString()
+            return amountSign + paid.toLocaleString()
         } } : null,
         { title: '关联退货单', dataIndex: ['refund', 'number'], },
     ]
