73e461ef7be46311186a99f226d59455c21fd821
Wendy <1155107790@link.cuhk.edu.hk>
2024-10-22 16:00:35 +0800

    ✨ feat: complete PartnerManager with 3 tables

--- src/components/PartnerManager/PartnerManager.js
+++ src/components/PartnerManager/PartnerManager.js
@@ -1,18 +1,28 @@
-import { Col, Row, Space } from 'antd'
+import { Col, Row, Space, Tabs } from 'antd'
 import React, { useEffect, useState } from 'react'
 import { partnerService } from '../../services'
-import PartnerTable from './PartnerTable'
+import { PartnerInvoiceItemTable, PartnerInvoiceTable, PartnerProductTable } from '../Table'
 
 
 const span = { xs: 24, sm: 12, md: 8, lg: 6 }
 
 
-const PartnerManager = ({ partner: initPartner }) => {
+/**
+ * @component
+ * @param {Object} props
+ * @param {Object} props.partner
+ * @param {string} props.partner.name
+ */
+const PartnerManager = (props) => {
+    const { partner: initPartner } = props
+    const { name: partnerName } = initPartner
+
     const [partner, setPartner] = useState({})
+    const [tabKey, setTabKey] = useState(0)
 
     const load = () => {
-        if (initPartner.name) {
-            partnerService.fetch(initPartner.name).then(res => {
+        if (partnerName) {
+            partnerService.fetch(partnerName).then(res => {
                 setPartner(res.data)
             }).catch(err => {
                 setPartner({})
@@ -22,14 +32,36 @@ const PartnerManager = ({ partner: initPartner }) => {
 
     useEffect(load, [initPartner])
 
+    const tabItems = [
+        {
+            label: '总览',
+            key: 0,
+            children: <PartnerInvoiceTable partner={partner} />,
+        },
+        {
+            label: '明细',
+            key: 1,
+            children: <PartnerInvoiceItemTable partner={partner} />,
+        },
+        {
+            label: '产品',
+            key: 2,
+            children: <PartnerProductTable partner={partner} />,
+        },
+    ]
+
+
     return <Space direction='vertical' style={{ width: '100%', paddingTop: '5px' }} size={15}>
-        <Row gutter={[8,8]} justify='start'>
-            <Col {...span}>姓名：{ partner.name }</Col>
-            <Col {...span}>电话：{ partner.phone }</Col>
-            <Col {...span}>地址：{ partner.address }</Col>
-            <Col {...span}>文件位置：{ partner.folder }</Col>
+        <Row gutter={[8, 8]} justify='start'>
+            <Col {...span}>姓名：{partner.name}</Col>
+            <Col {...span}>电话：{partner.phone}</Col>
+            <Col {...span}>地址：{partner.address}</Col>
+            <Col {...span}>文件位置：{partner.folder}</Col>
         </Row>
-        <PartnerTable partner={partner} />
+        <Tabs defaultActiveKey='1'
+            onChange={setTabKey}
+            activeKey={tabKey}
+            items={tabItems} />
     </Space>
 }
 
