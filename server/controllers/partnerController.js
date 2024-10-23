e1ceee2d2de45bd43190129e69c88de4d98bcbfa
Wendy <1155107790@link.cuhk.edu.hk>
2024-10-24 03:01:47 +0800

    🐛 fix: respond with correct `salesNum` & `purchaseNum` for partner list

--- server/controllers/partnerController.js
+++ server/controllers/partnerController.js
@@ -1,28 +1,36 @@
 const { Op } = require('sequelize')
 const BaseController = require('./baseController')
-const { Partner, Invoice, InvoiceItem, Product } = require('../models')
+const { Partner, Invoice, InvoiceItem, Product, sequelize } = require('../models')
 
 
 class PartnerController extends BaseController {
     index = async (req, res, next) => {
         try {
-            const { sortBy='name', order='ASC' } = req.query
+            const { sortBy = 'name', order = 'ASC' } = req.query
+
+            // Perf: wait for sequelize to support selecting from subQuery
+            // https://github.com/sequelize/sequelize/issues/5354
+            const salesNumQuery = `SELECT COUNT(id) AS salesNum 
+            FROM Invoices AS Invoice
+            WHERE Invoice.type LIKE 'sales%' AND Invoice.partnerName = Partner.name
+            GROUP BY partnerName`
+            const purchaseNumQuery = `SELECT COUNT(id) AS purchaseNum 
+            FROM Invoices AS Invoice
+            WHERE Invoice.type LIKE 'purchase%' AND Invoice.partnerName = Partner.name
+            GROUP BY partnerName`
+
             const options = {
-                include: {
-                    model: Invoice,
-                    as: 'invoices',
-                    attributes: ['type']
+                attributes: {
+                    include: [
+                        [sequelize.literal(`(${salesNumQuery})`), 'salesNum'],
+                        [sequelize.literal(`(${purchaseNumQuery})`), 'purchaseNum'],
+                    ]
                 },
                 group: ['Partner.name'],
                 order: [[sortBy, order]]
             }
+
             const partners = await Partner.findAll(options)
-            partners.forEach(p => {
-                const newPartner = p.dataValues
-                newPartner.salesNum = p.invoices.filter(i => i.type.includes('sales')).length
-                newPartner.purchaseNum = p.invoices.filter(i => i.type.includes('purchase')).length
-                delete newPartner.invoices
-            })
             req.partners = partners
             next()
         } catch (error) {
@@ -100,7 +108,7 @@ class PartnerController extends BaseController {
 
     destroy = async (req, res, next) => {
         try {
-            const options = { where: { name: req.params.name }}
+            const options = { where: { name: req.params.name } }
             const deletedCount = await Partner.destroy(options)  // 0 or 1
             if (deletedCount == 0) {
                 return this.handleNotFound(res)
