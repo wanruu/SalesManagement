9fa19508603adc0e5913d6aeac695b7157528db2
Wendy <1155107790@link.cuhk.edu.hk>
2024-10-24 18:26:22 +0800

    ✨ feat: allow editing partner in detail modal

--- src/components/PartnerManager/PartnerManager.js
+++ src/components/PartnerManager/PartnerManager.js
@@ -1,10 +1,10 @@
-import { Col, Row, Space, Tabs } from 'antd'
+import { Col, message, Row, Space, Tabs } from 'antd'
 import React, { useEffect, useState } from 'react'
 import { partnerService } from '../../services'
 import { PartnerInvoiceItemTable, PartnerInvoiceTable, PartnerProductTable } from '../Table'
+import { EditableItem } from '../Input'
+import { pick, isEqual } from 'lodash'
 
-
-const span = { xs: 24, sm: 12, md: 8, lg: 6 }
 
 
 /**
@@ -12,9 +12,10 @@ const span = { xs: 24, sm: 12, md: 8, lg: 6 }
  * @param {Object} props
  * @param {Object} props.partner
  * @param {string} props.partner.name
+ * @param {Function} [props.onPartnerChange]
  */
 const PartnerManager = (props) => {
-    const { partner: initPartner } = props
+    const { partner: initPartner, onPartnerChange } = props
     const { name: partnerName } = initPartner
 
     const [partner, setPartner] = useState({})
@@ -30,7 +31,12 @@ const PartnerManager = (props) => {
         }
     }
 
-    useEffect(load, [initPartner])
+    const handlePartnerChange = (newPartner) => {
+        onPartnerChange(newPartner)
+        setPartner({ ...partner, ...newPartner })
+    }
+
+    useEffect(load, [partnerName])
 
     const tabItems = [
         {
@@ -50,19 +56,58 @@ const PartnerManager = (props) => {
         },
     ]
 
-
     return <Space direction='vertical' style={{ width: '100%', paddingTop: '5px' }} size={15}>
-        <Row gutter={[8, 8]} justify='start'>
-            <Col {...span}>姓名：{partner.name}</Col>
-            <Col {...span}>电话：{partner.phone}</Col>
-            <Col {...span}>地址：{partner.address}</Col>
-            <Col {...span}>文件位置：{partner.folder}</Col>
-        </Row>
+        <PartnerInfo partner={partner} onPartnerChange={handlePartnerChange} />
         <Tabs defaultActiveKey='1'
             onChange={setTabKey}
             activeKey={tabKey}
             items={tabItems} />
     </Space>
+}
+
+
+const PartnerInfo = (props) => {
+    const { partner, onPartnerChange } = props
+
+    const items = [
+        { key: 'name', label: '姓名', name: 'name', },
+        { key: 'folder', label: '文件夹', name: 'folder' },
+        { key: 'phone', label: '电话', name: 'phone' },
+        { key: 'address', label: '地址', name: 'address' },
+    ]
+
+    const handleSave = (values) => {
+        const fieldnames = ['name', 'phone', 'address', 'folder']
+        const newPartner = pick(values, fieldnames)
+        if (!isEqual(newPartner, pick(partner, fieldnames))) {
+            const messageKey = 'update-partner'
+            message.open({
+                type: 'loading', duration: 86400,
+                content: '保存中', key: messageKey
+            })
+            partnerService.update(partner.name, newPartner).then(res => {
+                onPartnerChange(res.data)
+                message.open({ type: 'success', content: '保存成功', key: messageKey })
+            }).catch(err => {
+                message.open({
+                    type: 'error', duration: 5, key: messageKey,
+                    content: `保存失败：${err.message}. ${err.response?.data?.error}`,
+                })
+            })
+        }
+    }
+
+    return (
+        <Row gutter={[8, 8]} justify='start'>
+            {items.map(item => (
+                <Col xs={24} sm={12} md={8} lg={6}>
+                    <EditableItem record={partner} dataIndex={item.name} label={item.label}
+                        handleSave={handleSave} key={item.key} rules={item.rules}
+                        style={{ margin: 0 }} />
+                </Col>
+            ))}
+        </Row>
+    )
 }
 
 export default PartnerManager
