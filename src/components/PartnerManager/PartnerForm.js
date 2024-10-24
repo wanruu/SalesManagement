f7cc93f700424252227ec6926e9c1d9ac35e9f6a
Wendy <1155107790@link.cuhk.edu.hk>
2024-10-24 19:10:06 +0800

    ♻️ refactor: conform to form standard, delete redundant

--- src/components/PartnerManager/PartnerForm.js
+++ src/components/PartnerManager/PartnerForm.js
@@ -1,27 +1,46 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Button, message, Form, Input, Col, Space } from 'antd'
 import { partnerService } from '../../services'
 
-const { Item } = Form
 
-/*
-    Optional:
-        partner
-        onPartnerChange
-*/
-const PartnerForm = ({ partner, onPartnerChange }) => {
+const formItems = [
+    {
+        key: 'name', label: '姓名', dataIndex: 'name',
+        rules: [{ required: true }, { whitespace: true }]
+    },
+    {
+        key: 'folder', label: '文件夹', dataIndex: 'folder',
+        rules: [{ whitespace: true }]
+    },
+    {
+        key: 'phone', label: '电话', dataIndex: 'phone',
+        rules: [{ whitespace: true }]
+    },
+    {
+        key: 'address', label: '地址', dataIndex: 'address',
+        rules: [{ whitespace: true }]
+    }
+]
+
+
+/**
+ * @component
+ * @param {Object} props 
+ * @param {Object} props.partner
+ * @param {string} props.partner.name
+ * @param {string} [props.partner.phone]
+ * @param {string} [props.partner.address]
+ * @param {string} [props.partner.folder]
+ * @param {Function} [props.onPartnerChange]
+ */
+const PartnerForm = (props) => {
+    const { partner, onPartnerChange } = props
     const [form] = Form.useForm()
 
     const handleFinish = async () => {
-        const data = form.getFieldsValue(true)
-        const newPartner = {
-            name: data.name,
-            folder: data.folder,
-            phone: data.phone,
-            address: data.address,
-        }
-        const messageKey = 'partner'
-        message.open({ type: 'loading', key: messageKey, content: '提交中' })
+        const newPartner = form.getFieldsValue()
+        const messageKey = 'submit-partner'
+        message.open({ type: 'loading', key: messageKey, content: '保存中' })
         try {
             let res
             if (partner.name) {
@@ -35,61 +54,22 @@ const PartnerForm = ({ partner, onPartnerChange }) => {
             if (err.response?.data?.error == 'SequelizeUniqueConstraintError') {
                 message.open({ type: 'error', key: messageKey, content: '姓名重复' })
             } else {
-                message.open({ 
+                message.open({
                     type: 'error', key: messageKey, duration: 5,
-                    content: `${err.message}. ${err.response?.data?.error}` 
+                    content: `保存失败：${err.message}. ${err.response?.data?.error}`
                 })
             }
         }
     }
 
-    // rules
-    const nameRules = [
-        { required: true }, { whitespace: true },
-        { warningOnly: true, validator: async (rule, value) => {
-            if (partner.name && value !== partner.name) throw new Error()
-        }}
-    ]
-    const phoneRules = [
-        // { pattern: new RegExp('^[0-9]{11}$', 'g'), message: '电话不是11位数字', warningOnly: true },
-        { whitespace: true },
-        { warningOnly: true, validator: async (rule, value) => {
-            if (partner.name && value !== partner.phone) throw new Error()
-        }}
-    ]
-    const addressRules = [
-        { whitespace: true },
-        { warningOnly: true, validator: async (rule, value) => {
-            if (partner.name && value !== partner.address) throw new Error()
-        }}
-    ]
-    const folderRules = [
-        { whitespace: true },
-        { warningOnly: true, validator: async (rule, value) => {
-            if (partner.name && value !== partner.folder) throw new Error()
-        }}
-    ]
+    return <Form labelCol={{ span: 4 }} wrapperCol={{ span: 20 }} initialValues={partner}
+        onFinish={handleFinish} form={form}>
+        {formItems.map(item => (
+            <Form.Item key={item.key} label={item.label} name={item.dataIndex} rules={item.rules}>
+                <Input allowClear />
+            </Form.Item>
+        ))}
 
-    // initialize form
-    const resetForm = () => {
-        form.setFieldsValue(partner)
-    }
-    useEffect(resetForm, [])
-
-    return <Form labelCol={{ span: 4 }} wrapperCol={{ span: 20 }} 
-        onFinish={handleFinish} onReset={resetForm} form={form}>
-        <Item label='姓名' name='name' rules={nameRules}>
-            <Input allowClear />
-        </Item>
-        <Item label='文件位置' name='folder' rules={folderRules}>
-            <Input allowClear />
-        </Item>
-        <Item label='电话' name='phone' rules={phoneRules}>
-            <Input allowClear />
-        </Item>
-        <Item label='地址' name='address' rules={addressRules}>
-            <Input allowClear />
-        </Item>
         <Col align='middle'>
             <Space>
                 <Button htmlType='reset'>重置</Button>
