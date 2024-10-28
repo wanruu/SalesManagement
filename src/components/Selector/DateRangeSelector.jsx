06106e3b8746665fb0db17298d2c5367029b2a37
Louis LUO <ssparkluo@gmail.com>
2024-10-28 23:21:59 +0800

    ✨ feat(date-range-selector): bugfix, add picket type

--- src/components/Selector/DateRangeSelector.jsx
+++ src/components/Selector/DateRangeSelector.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { DatePicker } from 'antd';
+import { DatePicker, Space, Select } from 'antd';
 import dayjs from 'dayjs';
 import './selector.style.scss';
 
@@ -19,44 +19,71 @@ export default function DateRangeSelector({
 }) {
     const initValue = value || [null, null];
     const [dateRange, setDateRange] = useState(initValue);
+    const [rangeMode, setRangeMode] = useState('date');
 
     useEffect(() => {
         if (value === undefined) return;
+        if (value === null) {
+            setDateRange([null, null]);
+            return;
+        }
         if (value !== dateRange) {
             setDateRange(value);
         }
     }, [value]);
 
-    const valueOnChange = ([date1, date2]) => {
-        setDateRange([date1, date2]);
-        onChange([date1, date2]);
+    const handleChange = (value) => {
+        console.log(value);
+        if (value === null) {
+            setDateRange([null, null]);
+            onChange(null);
+        } else {
+            const [date1, date2] = value;
+            setDateRange([date1, date2]);
+            onChange([date1, date2]);
+        }
     };
 
-    const setRange = (type) => {
-        valueOnChange([ dayjs().startOf(type), dayjs().endOf(type) ])
+    const handleRangeSwitch = (type) => {
+        setRangeMode('date');
+        handleChange([ dayjs().startOf(type), dayjs().endOf(type) ])
     };
 
     return (
         <div className='daterange-selector-wrap'>
             <div className='daterange-selector'>
-                <a onClick={() => setRange('today')}>
+                <a onClick={() => handleRangeSwitch('today')}>
                     今日
                 </a>
-                <a onClick={() => setRange('week')}>
+                <a onClick={() => handleRangeSwitch('week')}>
                     本周
                 </a>
-                <a onClick={() => setRange('month')}>
+                <a onClick={() => handleRangeSwitch('month')}>
                     本月
                 </a>
-                <a onClick={() => setRange('year')}>
+                <a onClick={() => handleRangeSwitch('year')}>
                     本年
                 </a>
             </div>
-            <RangePicker
-                value={dateRange}
-                onChange={valueOnChange}
-                style={{ width: 256 }}
-            />
+            <Space.Compact>
+                <Select
+                    style={{ width: 70, textAlign: 'center' }}
+                    options={[
+                        { label: '日期', value: 'date' },
+                        { label: '周', value: 'week' },
+                        { label: '月', value: 'month' },
+                        { label: '年', value: 'year' }
+                    ]}
+                    value={rangeMode}
+                    onChange={setRangeMode}
+                />
+                <RangePicker
+                    picker={rangeMode}
+                    value={dateRange}
+                    onChange={handleChange}
+                    {...props}
+                />
+            </Space.Compact>
         </div>
     );
 }
